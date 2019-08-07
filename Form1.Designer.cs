4bdcb8a1c005a03338df2158bbedadfcf32e8030
John Han <mrht@qq.com>
2019-08-07 17:16:47 +0800

    v1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Calculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.number7 = new System.Windows.Forms.Button();
+            this.value = new System.Windows.Forms.Label();
+            this.number8 = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.ac = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.number6 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.Button();
+            this.number1 = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.neg = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number7
+            // 
+            this.number7.CausesValidation = false;
+            this.number7.Location = new System.Drawing.Point(16, 136);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(64, 36);
+            this.number7.TabIndex = 20;
+            this.number7.TabStop = false;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = true;
+            this.number7.Click += new System.EventHandler(this.number7_Click);
+            // 
+            // value
+            // 
+            this.value.BackColor = System.Drawing.Color.White;
+            this.value.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.value.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.value.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.value.Location = new System.Drawing.Point(16, 16);
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(292, 44);
+            this.value.TabIndex = 1;
+            this.value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // number8
+            // 
+            this.number8.Location = new System.Drawing.Point(92, 136);
+            this.number8.Name = "number8";
+            this.number8.Size = new System.Drawing.Size(64, 36);
+            this.number8.TabIndex = 2;
+            this.number8.TabStop = false;
+            this.number8.Text = "8";
+            this.number8.UseVisualStyleBackColor = true;
+            this.number8.Click += new System.EventHandler(this.number8_Click);
+            // 
+            // number9
+            // 
+            this.number9.Location = new System.Drawing.Point(168, 136);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(64, 36);
+            this.number9.TabIndex = 3;
+            this.number9.TabStop = false;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = true;
+            this.number9.Click += new System.EventHandler(this.number9_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(244, 136);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(64, 36);
+            this.sub.TabIndex = 4;
+            this.sub.TabStop = false;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // mul
+            // 
+            this.mul.Location = new System.Drawing.Point(244, 88);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(64, 36);
+            this.mul.TabIndex = 8;
+            this.mul.TabStop = false;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(168, 88);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(64, 36);
+            this.div.TabIndex = 7;
+            this.div.TabStop = false;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(92, 88);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(64, 36);
+            this.del.TabIndex = 6;
+            this.del.TabStop = false;
+            this.del.Text = "←";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // ac
+            // 
+            this.ac.Location = new System.Drawing.Point(16, 88);
+            this.ac.Name = "ac";
+            this.ac.Size = new System.Drawing.Size(64, 36);
+            this.ac.TabIndex = 5;
+            this.ac.TabStop = false;
+            this.ac.Text = "AC";
+            this.ac.UseVisualStyleBackColor = true;
+            this.ac.Click += new System.EventHandler(this.ac_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(244, 184);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(64, 36);
+            this.plus.TabIndex = 12;
+            this.plus.TabStop = false;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // number6
+            // 
+            this.number6.Location = new System.Drawing.Point(168, 184);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(64, 36);
+            this.number6.TabIndex = 11;
+            this.number6.TabStop = false;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = true;
+            this.number6.Click += new System.EventHandler(this.number6_Click);
+            // 
+            // number5
+            // 
+            this.number5.Location = new System.Drawing.Point(92, 184);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(64, 36);
+            this.number5.TabIndex = 10;
+            this.number5.TabStop = false;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = true;
+            this.number5.Click += new System.EventHandler(this.number5_Click);
+            // 
+            // number4
+            // 
+            this.number4.Location = new System.Drawing.Point(16, 184);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(64, 36);
+            this.number4.TabIndex = 9;
+            this.number4.TabStop = false;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = true;
+            this.number4.Click += new System.EventHandler(this.number4_Click);
+            // 
+            // number3
+            // 
+            this.number3.Location = new System.Drawing.Point(168, 232);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(64, 36);
+            this.number3.TabIndex = 15;
+            this.number3.TabStop = false;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = true;
+            this.number3.Click += new System.EventHandler(this.number3_Click);
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(92, 232);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(64, 36);
+            this.number2.TabIndex = 14;
+            this.number2.TabStop = false;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = true;
+            this.number2.Click += new System.EventHandler(this.number2_Click);
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(16, 232);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(64, 36);
+            this.number1.TabIndex = 13;
+            this.number1.TabStop = false;
+            this.number1.Text = "1";
+            this.number1.UseVisualStyleBackColor = true;
+            this.number1.Click += new System.EventHandler(this.number1_Click);
+            // 
+            // point
+            // 
+            this.point.Location = new System.Drawing.Point(168, 280);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(64, 36);
+            this.point.TabIndex = 18;
+            this.point.TabStop = false;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // number0
+            // 
+            this.number0.Location = new System.Drawing.Point(92, 280);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(64, 36);
+            this.number0.TabIndex = 17;
+            this.number0.TabStop = false;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = true;
+            this.number0.Click += new System.EventHandler(this.number0_Click);
+            // 
+            // neg
+            // 
+            this.neg.Location = new System.Drawing.Point(16, 280);
+            this.neg.Name = "neg";
+            this.neg.Size = new System.Drawing.Size(64, 36);
+            this.neg.TabIndex = 16;
+            this.neg.TabStop = false;
+            this.neg.Text = "-/+";
+            this.neg.UseVisualStyleBackColor = true;
+            this.neg.Click += new System.EventHandler(this.neg_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(244, 232);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(64, 84);
+            this.equal.TabIndex = 19;
+            this.equal.TabStop = false;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(324, 331);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.number0);
+            this.Controls.Add(this.neg);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.ac);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.number9);
+            this.Controls.Add(this.number8);
+            this.Controls.Add(this.value);
+            this.Controls.Add(this.number7);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button number7;
+        private System.Windows.Forms.Label value;
+        private System.Windows.Forms.Button number8;
+        private System.Windows.Forms.Button number9;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button ac;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button number6;
+        private System.Windows.Forms.Button number5;
+        private System.Windows.Forms.Button number4;
+        private System.Windows.Forms.Button number3;
+        private System.Windows.Forms.Button number2;
+        private System.Windows.Forms.Button number1;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button number0;
+        private System.Windows.Forms.Button neg;
+        private System.Windows.Forms.Button equal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate
+{
+    public partial class Form1 : Form
+    {
+        private double num1;
+        private double num2;
+        private int curOper;  // 1代表+，2代表-，3代表*，4代表/
+        private bool operStatus;  // 操作符状态，当上一个输入是操作符时为真
+        private bool equalStatus;  // 等号状态，当上一个输入是=时为真
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            num1 = 0;
+            num2 = 0;
+            curOper = 0;
+            operStatus = false;
+            equalStatus = false;
+        }
+
+        private void number0_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number1_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number3_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number4_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number8_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+            handleValue(((Button)sender).Text);
+        }
+
+        private void neg_Click(object sender, EventArgs e)
+        {
+            if (value.Text == "" || equalStatus || operStatus) value.Text = "-";
+            else if (value.Text == "-") value.Text = "";
+
+            if (equalStatus) curOper = 0;  // 如果上次一输入是=，再输入数字时相当于重新开始计算，curOper初始化
+            operStatus = false;  // 只要按了-/+运算符状态就为false
+            equalStatus = false;  // 只要按了-/+等号状态就为false
+        }
+
+        private void ac_Click(object sender, EventArgs e)
+        {
+            value.Text = "";
+            Form1_Load(null, null);
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            if (equalStatus || operStatus || conTainPoint() || valueIsNull() || tooLong()) return;
+            value.Text += ".";
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            string t = value.Text;
+            if (t.Length == 0 || equalStatus || operStatus) return;
+            value.Text = t.Substring(0, t.Length - 1);
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            handleOper(1);
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            handleOper(2);
+        }
+
+        private void mul_Click(object sender, EventArgs e)
+        {
+            handleOper(3);
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            handleOper(4);
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            if (sender != null && (operStatus || endWithPoint() || valueIsNull())) return;  // 按了运算符以及数字以后不能直接按=
+
+            if (equalStatus) num1 = double.Parse(value.Text);  // 上一个操作符是=时num1重新复制num2不变
+            else num2 = double.Parse(value.Text);  // 上一个操作符不是=时num1在运算符函数中赋值num2重新赋值
+
+            string temp = "";
+            switch (curOper) {
+                case 1:
+                    temp = (num1 + num2).ToString();
+                    break;
+                case 2:
+                    temp = (num1 - num2).ToString();
+                    break;
+                case 3:
+                    temp = (num1 * num2).ToString();
+                    break;
+                case 4:
+                    temp = (num1 / num2).ToString();
+                    break;
+            }
+            value.Text = temp.Length > 14 ? temp.Substring(0, 14) : temp;
+
+            if (sender != null) equalStatus = true;
+        }
+
+        public void handleValue(String num)
+        {
+            // 上一次按键为运算符或=的时候重新输入新数字，否则视为继续输入当前数字
+            if (!operStatus && !equalStatus)
+            {
+                if (tooLong()) return;
+                value.Text += num;
+            }
+            else value.Text = num;
+
+            if (equalStatus) curOper = 0;  // 如果上次一输入是=，再输入数字时相当于重新开始计算，curOper初始化
+            operStatus = false;  // 只要按了数字运算符状态就为false
+            equalStatus = false;  // 只要按了数字等号状态就为false
+        }
+
+        public void handleOper(int operNum) {
+            if (endWithPoint() || valueIsNull()) return;  // 输入的数字以.结尾则不允许输入运算符
+
+            if (!operStatus)
+            {  // 判断一下是否连续按下运算符
+                if (curOper != 0 && !equalStatus) equal_Click(null, null);  // 不是第一次计算的时候，需要求值
+                num1 = double.Parse(value.Text);  // 不论是否第一次计算，都要给num1赋值
+            }
+
+            operStatus = true;  //运算符状态变为真
+            equalStatus = false;  // 等号状态变为假
+            curOper = operNum;  // 当前运算符变更
+        }
+
+        public bool endWithPoint() {
+            return value.Text.EndsWith(".");
+        }
+
+        public bool conTainPoint() {
+            return value.Text.Contains(".");
+        }
+
+        public bool valueIsNull() {
+            return value.Text == "";
+        }
+
+        public bool tooLong() {
+            return value.Text.Length >= 14;
+        }
+    }
+}
